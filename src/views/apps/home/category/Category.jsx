25728e3f00ec7a39a9e7fc0b54cd3e518fa6a0a0
Samuel Tetteh <khobbylynx55@gmail.com>
2024-05-16 14:41:18 +0000

    Update Category.jsx

--- src/views/apps/home/category/Category.jsx
+++ src/views/apps/home/category/Category.jsx
@@ -11,21 +11,21 @@ const Category = () => {
   const dispatch = useDispatch()
 
   const store = useSelector((state) => state.ecommerce)
-
-  // const handleFilterChange = (key, value) => {
-  //   if (searchParams === '') {
-  //     if (searchParams.has(key)) {
-  //       searchParams.delete(key)
-  //       setSearchParams(searchParams)
-  //     }
-  //   } else {
-  //     navigate(`/shop?${key}=${value}`)
-  //   }
-  // }
+  const handleFilterChange = (key, value) => {
+    if (searchParams === '') {
+      if (searchParams.has(key)) {
+        searchParams.delete(key)
+        setSearchParams(searchParams)
+      }
+    } else {
+      navigate(`/shop?${key}=${value}`)
+    }
+  }
 
   const handleChange = (value) => {
     navigate('/shop')
     dispatch(getProducts({ ...store.params, q: value }))
+    console.log('@Store @ Category', value, store.params.q)
   }
 
   return (
@@ -34,8 +34,13 @@ const Category = () => {
         <div
           key={category.id}
           className='cat__card'
-          onClick={() => handleChange(`${category.title}`)}
-          // onClick={() => handleFilterChange('cat', `${category.title}`)}
+          // onClick={() => {
+          //   dispatch(getProducts({ ...store.params, q: `${category.title}` }))
+          //   // navigate('/shop')
+          //   console.log('@Store @ Category', store.params.q)
+          // }}
+          // onClick={() => handleChange(`${category.title}`)}
+          onClick={() => handleFilterChange('q', `${category.title}`)}
         >
           <img src={category.url} alt={category.title} />
           <p>{category.title}</p>
