1128004e45ec0af2590106c83bb8ef61f2c0bc65
Samuel Tetteh <khobbylynx55@gmail.com>
2024-06-18 23:51:54 +0000

    Update authentication.js

--- src/redux/authentication.js
+++ src/redux/authentication.js
@@ -1,10 +1,148 @@
 // ** Redux Imports
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 // ** UseJWT import to get config
-import useJwt from '@src/auth/jwt/useJwt'
+import { createUserWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
+import { auth, db, googleProvider } from '../configs/firebase'
+import { doc, setDoc } from 'firebase/firestore'
 
-const config = useJwt.jwtConfig
+// ** Utils
+import { ToastContentRegister, splitEmail } from '../utility/Utils'
+import toast from 'react-hot-toast'
+
+// ** REGISTER NEW USER
+export const handleRegisterUser = createAsyncThunk(
+  'appEcommerce/handleRegisterUser',
+  async (userData) => {
+    const { email, password } = userData
+    try {
+      const userCredentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      const user = userCredentials.user
+
+      const { email: authEmail, uid: userId, photoURL } = user
+      const { accessToken, refreshToken } = user.stsTokenManager
+      const { createdAt, lastLoginAt } = user.metadata
+      const { username } = splitEmail(authEmail)
+
+      const createdUserData = {
+        email: authEmail,
+        id: userId,
+        accessToken,
+        refreshToken,
+        role: 'client',
+        avatar: photoURL,
+        username,
+        createdAt,
+        lastLoginAt,
+      }
+
+      const localUserData = {
+        email: authEmail,
+        id: userId,
+        accessToken,
+        refreshToken,
+        role: 'client',
+        avatar: photoURL,
+        username,
+      }
+
+      const wishlistItems = []
+      const cartItems = []
+      const orders = []
+      const notifications = []
+
+      await setDoc(doc(db, 'profiles', userId), createdUserData)
+      await setDoc(doc(db, 'wishlist', userId), { wishlistItems })
+      await setDoc(doc(db, 'cart', userId), { cartItems })
+      await setDoc(doc(db, 'orders', userId), { orders })
+      await setDoc(doc(db, 'notifications', userId), { notifications })
+
+      console.log('User created success', user, createdUserData)
+      toast((t) => (
+        <ToastContentRegister
+          t={t}
+          role={localUserData.role}
+          name={localUserData.username}
+        />
+      ))
+
+      return localUserData
+    } catch (error) {
+      const errorCode = error.code
+      const errorMessage = error.message
+
+      console.log('Error creating user Account', errorCode, errorMessage)
+    }
+  }
+)
+
+// ** GOOGLE AUTH
+export const handleGoogleAuth = createAsyncThunk(
+  'appEcommerce/handleGoogleAuth',
+  async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider)
+      const user = result.user
+
+      const { email: authEmail, uid: userId, photoURL } = user
+      const { accessToken, refreshToken } = user.stsTokenManager
+      const { createdAt, lastLoginAt } = user.metadata
+      const { username } = splitEmail(authEmail)
+
+      const createdUserData = {
+        email: authEmail,
+        id: userId,
+        accessToken,
+        refreshToken,
+        role: 'client',
+        avatar: photoURL,
+        username,
+        createdAt,
+        lastLoginAt,
+      }
+
+      const localUserData = {
+        email: authEmail,
+        id: userId,
+        accessToken,
+        refreshToken,
+        role: 'client',
+        avatar: photoURL,
+        username,
+      }
+
+      const wishlistItems = []
+      const cartItems = []
+      const orders = []
+      const notifications = []
+
+      await setDoc(doc(db, 'profiles', userId), createdUserData)
+      await setDoc(doc(db, 'wishlist', userId), { wishlistItems })
+      await setDoc(doc(db, 'cart', userId), { cartItems })
+      await setDoc(doc(db, 'orders', userId), { orders })
+      await setDoc(doc(db, 'notifications', userId), { notifications })
+
+      toast((t) => (
+        <ToastContentRegister
+          t={t}
+          role={localUserData.role}
+          name={localUserData.username}
+        />
+      ))
+
+      return localUserData
+    } catch (error) {
+      const errorCode = error.code
+      const errorMessage = error.message
+      console.log('Error', errorMessage, errorCode)
+      setErrorMsg(errorMessage)
+    }
+  }
+)
 
 const initialUser = () => {
   const item = window.localStorage.getItem('userData')
@@ -15,27 +153,31 @@ const initialUser = () => {
 export const authSlice = createSlice({
   name: 'authentication',
   initialState: {
-    userData: initialUser()
+    userData: initialUser(),
   },
   reducers: {
     handleLogin: (state, action) => {
       state.userData = action.payload
-      state[config.storageTokenKeyName] = action.payload[config.storageTokenKeyName]
-      state[config.storageRefreshTokenKeyName] = action.payload[config.storageRefreshTokenKeyName]
       localStorage.setItem('userData', JSON.stringify(action.payload))
-      localStorage.setItem(config.storageTokenKeyName, JSON.stringify(action.payload.accessToken))
-      localStorage.setItem(config.storageRefreshTokenKeyName, JSON.stringify(action.payload.refreshToken))
     },
-    handleLogout: state => {
+    handleLogout: (state) => {
       state.userData = {}
-      state[config.storageTokenKeyName] = null
-      state[config.storageRefreshTokenKeyName] = null
+
       // ** Remove user, accessToken & refreshToken from localStorage
       localStorage.removeItem('userData')
-      localStorage.removeItem(config.storageTokenKeyName)
-      localStorage.removeItem(config.storageRefreshTokenKeyName)
-    }
-  }
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(handleRegisterUser.fulfilled, (state, action) => {
+        state.userData = action.payload
+        localStorage.setItem('userData', JSON.stringify(action.payload))
+      })
+      .addCase(handleGoogleAuth.fulfilled, (state, action) => {
+        state.userData = action.payload
+        localStorage.setItem('userData', JSON.stringify(action.payload))
+      })
+  },
 })
 
 export const { handleLogin, handleLogout } = authSlice.actions
