332d9d0efc503761f36f719a1df4d25257340d8a
Samuel Tetteh <khobbylynx55@gmail.com>
2024-06-18 23:51:51 +0000

    Update rootReducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,20 +2,12 @@
 import navbar from './navbar'
 import layout from './layout'
 import auth from './authentication'
-import chat from '@src/views/apps/chat/store'
-import users from '@src/views/apps/user/store'
-import email from '@src/views/apps/email/store'
-import invoice from '@src/views/apps/invoice/store'
 import ecommerce from '@src/views/apps/ecommerce/store'
 
 const rootReducer = {
   auth,
-  chat,
-  email,
-  users,
   navbar,
   layout,
-  invoice,
   ecommerce,
 }
 
