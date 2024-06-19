32b81b607b8e427b955476582bea24333d59b3f0
Samuel Tetteh <khobbylynx55@gmail.com>
2024-06-19 23:52:11 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -126,6 +126,17 @@ export default () => {
     },
     build: {
       rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id
+                .toString()
+                .split('node_modules/')[1]
+                .split('/')[0]
+                .toString()
+            }
+          },
+        },
         plugins: [rollupNodePolyFill()],
       },
     },
