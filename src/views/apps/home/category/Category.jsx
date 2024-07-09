a41cc275ed3a08d2d0a18349e53bd55e2e079f15
Samuel Tetteh <khobbylynx55@gmail.com>
2024-07-09 08:20:27 +0000

    Update Category.jsx

--- src/views/apps/home/category/Category.jsx
+++ src/views/apps/home/category/Category.jsx
@@ -1,31 +1,18 @@
 import React from 'react'
 import './Category.scss'
 import { categories } from '../data'
-import { useNavigate, useSearchParams } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { getProducts } from '../../ecommerce/store'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { selectCategory, filterByCategory } from '../../ecommerce/store'
 
 const Category = () => {
-  const [searchParams, setSearchParams] = useSearchParams()
   const navigate = useNavigate()
   const dispatch = useDispatch()
 
-  const store = useSelector((state) => state.ecommerce)
-  const handleFilterChange = (key, value) => {
-    if (searchParams === '') {
-      if (searchParams.has(key)) {
-        searchParams.delete(key)
-        setSearchParams(searchParams)
-      }
-    } else {
-      navigate(`/shop?${key}=${value}`)
-    }
-  }
-
-  const handleChange = (value) => {
+  const handleCategoryFilter = (category) => {
+    dispatch(selectCategory(category))
+    dispatch(dispatch(filterByCategory(category)))
     navigate('/shop')
-    dispatch(getProducts({ ...store.params, q: value }))
-    console.log('@Store @ Category', value, store.params.q)
   }
 
   return (
@@ -34,13 +21,7 @@ const Category = () => {
         <div
           key={category.id}
           className='cat__card'
-          // onClick={() => {
-          //   dispatch(getProducts({ ...store.params, q: `${category.title}` }))
-          //   // navigate('/shop')
-          //   console.log('@Store @ Category', store.params.q)
-          // }}
-          // onClick={() => handleChange(`${category.title}`)}
-          onClick={() => handleFilterChange('q', `${category.title}`)}
+          onClick={() => handleCategoryFilter(category.title)}
         >
           <img src={category.url} alt={category.title} />
           <p>{category.title}</p>
