d09f2fd3c3af89a04fc75a61caeb79554f4c97d0
Samuel Tetteh <khobbylynx55@gmail.com>
2024-08-13 22:29:04 +0000

    Pop up confirmation modal

---
+++ src/views/apps/ecommerce/checkout/ConfirmationModal.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
+
+const ModalConfig = [
+  {
+    id: 1,
+    title: 'Primary',
+    modalColor: 'modal-primary',
+    btnColor: 'primary',
+  },
+]
+
+const ConfirmationModal = ({
+  id,
+  title = 'Confirm',
+  msg,
+  btnText1,
+  btnText2,
+  modalColor = 'modal-danger',
+  btnColor = 'danger',
+  confirmFunction,
+  modal,
+  setModal,
+}) => {
+  const handleAccept = (id) => {
+    console.log('IDDD', id)
+    confirmFunction(id)
+    setModal(!modal)
+  }
+
+  return (
+    <div className={'theme-{modalColor}'}>
+      <Modal
+        isOpen={modal}
+        toggle={() => setModal(!modal)}
+        className='modal-dialog-centered'
+        modalClassName={modalColor}
+      >
+        <ModalHeader toggle={() => setModal(!modal)}>{title}</ModalHeader>
+        <ModalBody>{msg}</ModalBody>
+        <ModalFooter>
+          <Button color={btnColor} onClick={() => setModal(!modal)}>
+            {btnText1}
+          </Button>
+          <Button color={btnColor} onClick={() => handleAccept(id)}>
+            {btnText2}
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  )
+}
+export default ConfirmationModal
