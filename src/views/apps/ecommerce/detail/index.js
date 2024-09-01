dcdfca0fab4f402f18a6eb543c012de557ba1d26
Samuel Tetteh <khobbylynx55@gmail.com>
2024-09-01 04:13:52 +0000

    Add Product Not Found Page

--- src/views/apps/ecommerce/detail/index.js
+++ src/views/apps/ecommerce/detail/index.js
@@ -1,6 +1,6 @@
 // ** React Imports
 import { useEffect, Fragment } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 // ** Product detail components
 import ItemFeatures from './ItemFeatures'
@@ -23,15 +23,20 @@ import {
 } from '../store'
 
 import '@styles/base/pages/app-ecommerce-details.scss'
+import NotFound from './NotFound'
 
 const Details = () => {
   // ** Vars
   const { product: productId } = useParams()
-  // const productId = params.substring(params.lastIndexOf('-') + 1)
 
   // ** Store Vars
   const dispatch = useDispatch()
   const store = useSelector((state) => state.ecommerce)
+
+  const navigate = useNavigate()
+
+  // Verify Product Id
+  const found = store.allProducts.find((pro) => pro.id === productId)
 
   // ** ComponentDidMount : Get product
   useEffect(() => {
@@ -39,42 +44,48 @@ const Details = () => {
   }, [dispatch, productId])
 
   return (
-    <Fragment>
-      <BreadCrumbs
-        title='Product Details'
-        data={[
-          { title: 'Shop' },
-          {
-            title: `${
-              store.productDetail.name === undefined
-                ? ''
-                : store.productDetail.name
-            }`,
-          },
-        ]}
-      />
-      <div className='app-ecommerce-details'>
-        {Object.keys(store.productDetail).length ? (
-          <Card>
-            <CardBody>
-              <ProductDetails
-                dispatch={dispatch}
-                addToCart={addToCart}
-                productId={productId}
-                getProduct={getProduct}
-                data={store.productDetail}
-                addToWishlist={addToWishlist}
-                deleteWishlistItem={deleteWishlistItem}
-              />
-            </CardBody>
-            <ItemFeatures />
-            <CardBody>
-              <RelatedProducts dispatch={dispatch} store={store} />
-            </CardBody>
-          </Card>
-        ) : null}
-      </div>
-    </Fragment>
+    <>
+      {!found ? (
+        <NotFound />
+      ) : (
+        <Fragment>
+          <BreadCrumbs
+            title='Product Details'
+            data={[
+              { title: 'Shop' },
+              {
+                title: `${
+                  store.productDetail.name === undefined
+                    ? ''
+                    : store.productDetail.name
+                }`,
+              },
+            ]}
+          />
+          <div className='app-ecommerce-details'>
+            {Object.keys(store.productDetail).length ? (
+              <Card>
+                <CardBody>
+                  <ProductDetails
+                    dispatch={dispatch}
+                    addToCart={addToCart}
+                    productId={productId}
+                    getProduct={getProduct}
+                    data={store.productDetail}
+                    addToWishlist={addToWishlist}
+                    deleteWishlistItem={deleteWishlistItem}
+                  />
+                </CardBody>
+                <ItemFeatures />
+                <CardBody>
+                  <RelatedProducts dispatch={dispatch} store={store} />
+                </CardBody>
+              </Card>
+            ) : null}
+          </div>
+        </Fragment>
+      )}
+    </>
   )
 }
 
